81b498698413551aa1a2454692eb74be64940916
imthequeen-royal <131184289+imthequeen-royal@users.noreply.github.com>
2023-04-18 17:43:11 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,25 @@
+input.onButtonPressed(Button.AB, function () {
+	
+})
+input.onGesture(Gesture.Shake, function () {
+	
+})
+music.playMelody("E A E F E C E C ", 500)
+led.plot(1, 0)
+led.unplot(0, 0)
+led.plotBarGraph(
+0,
+0
+)
+null.turn(Direction.Right, 45)
+basic.showLeds(`
+    . # . # .
+    # . # . #
+    # . # . #
+    . # # # .
+    . . # . .
+    `)
+basic.showString("I'm microbit")
+basic.forever(function () {
+	
+})
